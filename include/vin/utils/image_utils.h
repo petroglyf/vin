bc12ba5e52a297ddbf383dda93317d646901f879
Nick DePalma <ndepalma@gmail.com>
2023-05-07 20:12:34 -0400

    Removing superfluous files

--- include/vin/utils/image_utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <functional_dag/dlpack.h>
-
-namespace image_utils {
-  /** 
-   * Load an image from a file and return a DLPack tensor.
-   */
-  DLTensor loadFile(const char * const filename);
-}
