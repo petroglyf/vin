9034ebe9fa5acac63cf660dc533e3b2d71756376
ND <ndepalma@gmail.com>
2025-04-15 02:19:50 -0400

    A few cleanup issues and style related (#37)

--- src/qtio/imageview_output.cpp
+++ src/qtio/imageview_output.cpp
@@ -10,7 +10,7 @@
 
 using namespace vin;
 
-QtViewer::QtViewer() : frame(nullptr), imagePanel(nullptr) {
+QtViewer::QtViewer() : m_frame(nullptr), m_imagePanel(nullptr) {
   // qApp is reserved and is inherited from QLabel
   QMetaObject::invokeMethod(qApp, [this]() {
     this->start();  // Start the widget thread so the callbacks work.
@@ -18,35 +18,35 @@ QtViewer::QtViewer() : frame(nullptr), imagePanel(nullptr) {
 }
 
 QtViewer::~QtViewer() {
-  if (frame != nullptr) {
-    frame->close();
-    frame->destroyed();
-    delete frame;
-    frame = nullptr;
-    imagePanel = nullptr;
+  if (m_frame != nullptr) {
+    m_frame->close();
+    m_frame->destroyed();
+    delete m_frame;
+    m_frame = nullptr;
+    m_imagePanel = nullptr;
   }
 }
 
 void QtViewer::start() {
-  frame = new QFrame();
-  QHBoxLayout *lay = new QHBoxLayout(frame);
-  imagePanel = new image_view();
-  lay->addWidget(imagePanel);
+  m_frame = new QFrame();
+  QHBoxLayout *lay = new QHBoxLayout(m_frame);
+  m_imagePanel = new image_view();
+  lay->addWidget(m_imagePanel);
 
-  frame->setMinimumSize(800, 600);
-  frame->show();
+  m_frame->setMinimumSize(800, 600);
+  m_frame->show();
 }
 
 std::unique_ptr<int> QtViewer::update(const arrow::Tensor *image) {
-  if (imagePanel == nullptr) return nullptr;
+  if (m_imagePanel == nullptr) return nullptr;
   QWidgetList widglist = QApplication::topLevelWidgets();
   if (image != nullptr && image->shape().size() == 3) {
     switch (image->shape()[0]) {
       case 1:
-        imagePanel->set_tensor(*image, visualization_mode::VIZ_HEATMAP);
+        m_imagePanel->set_tensor(*image, visualization_mode::VIZ_HEATMAP);
         break;
       case 3:
-        imagePanel->set_tensor(*image, visualization_mode::VIZ_RGB);
+        m_imagePanel->set_tensor(*image, visualization_mode::VIZ_RGB);
         break;
       default:
         LOG(WARNING) << "Unable to visualize image with n_channels: "
@@ -65,10 +65,10 @@ std::unique_ptr<int> QtViewer::update(const arrow::Tensor *image) {
         uint32_t y = image->Value<arrow::UInt32Type>(f);
         goal_points.push_back(std::make_tuple(x, y));
       }
-      imagePanel->set_gaze_pts(goal_points);
+      m_imagePanel->set_gaze_pts(goal_points);
     } else {
       // Each row is a bounding box
-      imagePanel->clear_overlay();
+      m_imagePanel->clear_overlay();
       for (int64_t row = 0; row < image->shape()[0]; row++) {
         f[0] = row;
         f[1] = 0;
@@ -79,7 +79,7 @@ std::unique_ptr<int> QtViewer::update(const arrow::Tensor *image) {
         int width = image->Value<arrow::UInt32Type>(f);
         f[1] = 3;
         int height = image->Value<arrow::UInt32Type>(f);
-        imagePanel->draw_box(x, y, width, height);
+        m_imagePanel->draw_box(x, y, width, height);
       }
     }
   } else
