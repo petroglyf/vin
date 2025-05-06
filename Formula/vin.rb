88e9fb42711cdafd979de6d787bb287267fc8212
Nick DePalma <ndepalma@gmail.com>
2025-05-05 23:18:05 -0400

    Updating instructions and fixing a few last minute metadata items

--- Formula/vin.rb
+++ Formula/vin.rb
@@ -3,43 +3,31 @@ require "formula"
 class Vin < Formula
   desc "Executable for managing filtergraphs for vision"
   homepage "https://github.com/petrogly-ph/vin"
-  url "https://github.com/petrogly-ph/vin/archive/refs/tags/v0.5pre.release.tar.gz"
-  # url "file:///Users/drobotnik/projects/vin/"
-  sha256 "0d7aefe30432f9d036d98b36806a7e3a3e41032518c2c35ae222d9c98f55ab0f"
+  url "https://github.com/petroglyf/vin/archive/refs/tags/brew-v1.0.tar.gz"
+  sha256 "bd6b97311ccb1cad7556291fb2e8014982f5bd32249af2a6d257508c6dc39817"
   license "MIT"
-  version "0.7"
-
-  # bottle do
-  #   cellar :any
-  #   sha1 "5466fbee57b366a41bbcec814614ee236e39bed8" => :yosemite
-  #   sha1 "bde270764522e4a1d99767ca759574a99485e5ac" => :mavericks
-  #   sha1 "e77d0e5f516cb41ac061e1050c8f37d0fb65b796" => :mountain_lion
-  # end
+  version "1.0rc1"
 
   depends_on "cmake" => :build
-  depends_on "qt@6" => :build
-  depends_on "functional-dag" => :build
-  depends_on "clang-tidy" => :build
-
+  depends_on "meson" => :build
   depends_on "catch2" => :test
+  depends_on "ninja" => :build
+  depends_on "qt@6" => :build
+  depends_on "flatbuffers" => :build
+  depends_on "apache-arrow" => :build
+  depends_on "glog" => :build
+  depends_on "functional-dag" => :build
+  depends_on "clang-tidy" => :test
+
+  
   
   def install
-    # ENV.cxx20 if build.cxx20?
-    mkdir "vin-build" do
-      args = std_cmake_args
-      
-      system "cmake", "..", *args
-      system "make", "install"
-    end
-
-
-    # ENV.deparallelize
-    # system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    # system "make", "install"
+    system "meson", "setup", "build", *std_meson_args
+    system "ninja", "-C", "build"
+    system "meson", "install", "-C", "build"
   end
 
-  # test do
-  #   system "false"
-  # end
+  test do
+    system "false"
+  end
 end
